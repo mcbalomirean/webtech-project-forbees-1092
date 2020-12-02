80509245ca3efe32c2dff732e4e536d4ab568e05
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2020-12-02 15:50:16 +0200

    Oops, fixed status messages.

--- api/routes/test.js
+++ api/routes/test.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var db = require('../models/database');
 
 router.get('/api', function(req, res, next) {
-    res.send('Express API is functional.').status(200);
+    res.status(200).send('Express API is functional.');
 });
 
 router.get('/database/creation', function(req, res, next) {
-    db.sequelize.sync( { force: true }).then(/* async */ () => {
-        res.send('Database tables created.').status(201);
+    db.sequelize.sync( { force: true } ).then(() => {
+        res.status(201).send('Database tables created.');
     }).catch((err) => {
-        res.send('Database tables not created.').status(500);
-        console.error(err);
+        res.status(500).send('Database tables not created.');
+        console.log(err);
     });
 });
 
