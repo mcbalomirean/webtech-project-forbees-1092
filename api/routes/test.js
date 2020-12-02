076639a887a09b9b9e13e7b4b6276104554e3d23
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2020-12-02 17:22:47 +0200

    Updated test suite to use async await.

--- api/routes/test.js
+++ api/routes/test.js
@@ -6,13 +6,14 @@ router.get('/api', function(req, res, next) {
     res.status(200).send('Express API is functional.');
 });
 
-router.get('/database/creation', function(req, res, next) {
-    db.sequelize.sync( { force: true } ).then(() => {
+router.get('/database/creation', async function(req, res, next) {
+    try {
+        await db.sequelize.sync({ force: true });
         res.status(201).send('Database tables created.');
-    }).catch((err) => {
+    } catch (error) {
         res.status(500).send('Database tables not created.');
-        console.log(err);
-    });
+        console.log(error);
+    }
 });
 
 module.exports = router;
