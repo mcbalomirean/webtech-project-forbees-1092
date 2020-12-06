828c291324b48023e8e835372689fc7ad5fc587d
Bogdan Bebereche <bogdanbebereche@gmail.com>
2020-12-06 15:27:30 +0200

    Added for groups controller

--- api/routes/groups.js
+++ api/routes/groups.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const GroupsController = require('../controllers/GroupsController.js')
+const GroupsController = require('../controllers/GroupsController')
 
 router.get('/', GroupsController.findAll)
 router.get('/:id', GroupsController.findOne)
 // router.get('/:id/groupMembers', GroupsController.findGroupMembers) // TODO: doesn't exist in the controller, throws error!
+
+router.post('/', GroupsController.create)
+
 
 module.exports = router;
