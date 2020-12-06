2f2cdc0562b390e4be9eb284532dc5c3a52e01ad
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2020-12-07 01:41:48 +0200

    Moved SideBar into its own component.

--- notebee/src/containers/App.js
+++ notebee/src/containers/App.js
@@ -1,26 +1,14 @@
-import React, { Fragment, useState, useCallback } from 'react'; // TODO: add state?
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton, Drawer, Hidden, Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { 
-  Menu as MenuIcon, 
-  AccountCircle as AccountCircleIcon,
-  LibraryBooks as LibraryBooksIcon,
-  SupervisedUserCircle as SupervisedUserCircleIcon,
-  Subject as SubjectIcon } from '@material-ui/icons';
-import MainView from './MainView';
+import React, { useState, useCallback } from 'react'; // TODO: add state?
+import { BrowserRouter as Router, } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import { Menu as MenuIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles'
-
-const drawerWidth = 200;
+import MainView from './MainView';
+import Sidebar from '../components/SideBar';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex'
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
   },
   drawerButton: {
     marginRight: theme.spacing(2),
@@ -28,17 +16,13 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  toolbar: theme.mixins.toolbar,
+  toolbar: theme.mixins.toolbar, // TODO: maybe use this for a sticky appbar
   content: {
     flexGrow: 1
   },
 }));
 
-export default function App(props) {
-  const { window } = props;
+export default function App() {
   const classes = useStyles();
 
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -46,78 +30,10 @@ export default function App(props) {
     setMobileOpen(!mobileOpen);
   }, [mobileOpen]);
 
-  const drawerContents = (
-    <Fragment>
-      <div className={classes.toolbar} /> {/* This adds an offset of the size of the toolbar for aesthetic reasons, before the drawer contents. */}
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <AccountCircleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <SupervisedUserCircleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Groups"/>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <SubjectIcon />
-          </ListItemIcon>
-          <ListItemText primary="Notes"/>
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <LibraryBooksIcon />
-          </ListItemIcon>
-          <ListItemText primary="Notebook"/>
-        </ListItem>
-      </List>
-    </Fragment>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
   return (
     <Router>
       <div className={classes.root}>
-        <nav className={classes.drawer}>
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant='temporary'
-            anchor='left'
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile according to Material-UI developers.
-            }}
-          >
-            {drawerContents}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant='permanent'
-            anchor='left'
-            open
-          >
-            {drawerContents}
-          </Drawer>
-        </Hidden>
-        </nav>
+        <Sidebar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle}/>
         <main className={classes.content}>
           <AppBar position="static">
             <Toolbar>
