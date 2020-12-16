48b403f2aca6f825af61b8cc1e3bb7b366bc35f0
Dumimoto <adrian.barna31@gmail.com>
2020-12-16 22:58:35 +0200

    Made the App Bar as a separate component named MainAppBar.js and made the required changes to App.js

--- notebee/src/containers/App.js
+++ notebee/src/containers/App.js
@@ -1,24 +1,23 @@
-import React, { useState, useCallback } from 'react'; // TODO: add state?
-import { BrowserRouter as Router, } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton } from '@material-ui/core';
-import { Menu as MenuIcon } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles'
-import MainView from './MainView';
-import Sidebar from '../components/SideBar';
+import React, { useState, useCallback } from "react"; // TODO: add state?
+import { BrowserRouter as Router } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import MainAppBar from "../components/MainAppBar";
+import MainView from "./MainView";
+import Sidebar from "../components/SideBar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex'
+    display: "flex",
   },
   drawerButton: {
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
     },
   },
   toolbar: theme.mixins.toolbar, // TODO: maybe use this for a sticky appbar
   content: {
-    flexGrow: 1
+    flexGrow: 1,
   },
 }));
 
@@ -33,18 +32,15 @@ export default function App() {
   return (
     <Router>
       <div className={classes.root}>
-        <Sidebar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle}/>
+        <Sidebar
+          mobileOpen={mobileOpen}
+          handleDrawerToggle={handleDrawerToggle}
+        />
         <main className={classes.content}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton className={classes.drawerButton} edge='start' color='inherit' aria-label='open drawer' onClick={handleDrawerToggle}>
-                <MenuIcon />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
+          <MainAppBar handleDrawerToggle={handleDrawerToggle} />
           <MainView />
         </main>
       </div>
     </Router>
   );
-};
+}
