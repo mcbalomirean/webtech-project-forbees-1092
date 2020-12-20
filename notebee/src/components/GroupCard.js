7332a57d9aed0c781ff3b80c9d6cdad230ac32ec
Bogdan Bebereche <bogdanbebereche@gmail.com>
2020-12-20 17:10:37 +0200

    Added functionality to groups buttons

--- notebee/src/components/GroupCard.js
+++ notebee/src/components/GroupCard.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+
+import axios from "axios";
 
 const useStyles = makeStyles({
   root: {
@@ -25,23 +27,55 @@ const useStyles = makeStyles({
 
 export default function GroupCard(props) {
   const classes = useStyles();
+  const [students, setStudents] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loaded, setLoaded] = useState(true);
+  const [isDeleted, setIsDeleted] = useState(false);
 
-  return (
+  const DisplayStudents = () => {
+    const API = process.env.REACT_APP_API_BASEURL;
+    const id = props.group.id;
+    axios.get(API + "/groups/members/" + id).then((result) => {
+      setStudents(result.data.results.students);
+      setLoaded(false);
+    });
+    setIsOpen(!isOpen);
+    if (isOpen == false) setStudents([]);
+  };
+
+  const DeleteGroup = () => {
+    const API = process.env.REACT_APP_API_BASEURL;
+    const id = props.group.id;
+    axios.delete(API + "/groups/" + id);
+    setIsDeleted(true);
+  };
+
+  return isDeleted ? null : (
     <Card className={classes.root}>
       <CardContent>
         <Typography variant="h5" component="h2">
           Group: {props.group.name}
           <br />
         </Typography>
-
-        {props.group.students.map((student) => (
-          <Typography>{props.group.students.email}</Typography>
-        ))}
       </CardContent>
       <CardActions>
-        <Button size="small">See group members</Button>
-        <Button size="small">Delete Group</Button>
+        <Button size="small" onClick={DisplayStudents}>
+          See group members
+        </Button>
+        <Button size="small" onClick={DeleteGroup}>
+          Delete Group
+        </Button>
       </CardActions>
+      {isOpen ? (
+        students.map((student) => (
+          <Typography>
+            {student.email}
+            <br />
+          </Typography>
+        ))
+      ) : (
+        <Typography> </Typography>
+      )}
     </Card>
   );
 }
--- notebee/src/containers/Groups.js
+++ notebee/src/containers/Groups.js
@@ -7,13 +7,10 @@ import axios from "axios";
 const API = process.env.REACT_APP_API_BASEURL;
 
 export default function Groups() {
-  //assignments: []
   const [groups, setGroups] = useState([]);
-  const [students, setStudents] = useState([]);
 
   useEffect(() => {
     axios.get(API + "/groups").then((result) => {
-      // axios.get("/groups").then((result) => {
       setGroups(result.data.results);
     });
   }, []);
