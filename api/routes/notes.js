842d05c9164d734ca7534f45d9d30e9b136b313a
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2020-12-21 00:28:47 +0200

    rename notes controller

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const NotesController = require('../controllers/NotesControllers');
+const NotesController = require("../controllers/NotesController");
 
-router.post('/', NotesController.create);
+router.post("/", NotesController.create);
 
-router.get('/', NotesController.findAll);
-router.get('/:id', NotesController.findOne);
+router.get("/", NotesController.findAll);
+router.get("/:id", NotesController.findOne);
 
-router.delete('/:id', NotesController.delete);
+router.delete("/:id", NotesController.delete);
 
-router.put('/:id', NotesController.put);
+router.put("/:id", NotesController.put);
 
 module.exports = router;
