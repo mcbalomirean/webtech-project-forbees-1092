7e199ebcaf3f299441748d7c92539e3e68603838
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-01-16 00:49:23 +0200

    updated fontsource

--- notebee/src/index.js
+++ notebee/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CssBaseline } from "@material-ui/core";
-import "fontsource-roboto";
+import "@fontsource/roboto";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
