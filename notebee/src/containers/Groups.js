3bc06f6a0e3d6edbf1c677ff74aea851f3a74693
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-01-17 00:10:08 +0200

    Assorted changes to groups.

--- notebee/src/containers/Groups.js
+++ notebee/src/containers/Groups.js
@@ -1,54 +1,66 @@
-import React, { useState, useEffect } from "react";
-import GroupCard from "../components/GroupCard";
+import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useAuth } from "../hooks/useAuth";
-import { Typography } from "@material-ui/core";
-
-//Sets the base URL for requests
-const API = process.env.REACT_APP_API_BASEURL;
+import GroupCard from "../components/GroupCard";
+import { API } from "../util/constants";
 
 //Configuration in order to use users' credentials
 const config = {
-  baseURL: `${API}`,
+  baseURL: `${API}/groups`,
   withCredentials: true,
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1, 1, 1, 1),
+    width: "auto",
+  },
+}));
 
 export default function Groups() {
   //Used in order to use its hook to see if the user is logged in or not
   const auth = useAuth();
+  const classes = useStyles();
   //Hook to store all of the groups
   const [groups, setGroups] = useState([]);
 
   //saves all the groups form the database when the page loads
   useEffect(() => {
-    axios.get(API + "/groups", config).then((result) => {
+    axios.get("/", config).then((result) => {
       setGroups(result.data);
     });
   }, []);
 
   //Function which deletes a group then sets the content to the modified one
-  async function DeleteGroup(group) {
-    const id = group.id;
-    await axios.delete(API + "/groups/" + id, config);
-    await axios.get(API + "/groups", config).then((result) => {
-      setGroups(result.data);
-    });
+  async function handleDelete(groupId) {
+    await axios.delete(`/${groupId}`, config);
+    setGroups(
+      groups.filter((group) => {
+        return group.id !== groupId;
+      })
+    );
   }
 
   //what is shown on the page, which depends if the user is logged in or not
   return (
-    <div>
-      <br />
-      <br />
+    <Fragment>
       {auth.user ? (
-        groups.map((group) => (
-          <GroupCard group={group} id={group.id} DeleteGroup={DeleteGroup} />
-        ))
+        <Grid className={classes.root} container spacing={2}>
+          {groups.map((group) => (
+            <GroupCard
+              group={group}
+              id={group.id}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </Grid>
       ) : (
         <Typography variant="h4" align="center" style={{ padding: "10px" }}>
-          Please Log in to see your groups!
+          Please log in to see your groups!
         </Typography>
       )}
-    </div>
+    </Fragment>
   );
 }
